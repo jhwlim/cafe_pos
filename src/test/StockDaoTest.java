2a6b68198d1a63d1fea1f6e1b7c616c790f1f344
jh Lim <jhwlim25@gmail.com>
2020-12-23 09:17:57 +0900

    재고 수량 테스트 데이터 입력을 위한 StockDaoTest 클래스 작성

--- src/test/StockDaoTest.java
+++ src/test/StockDaoTest.java
@@ -3,6 +3,7 @@ package test;
 import common.model.StockVO;
 import stock.dao.StockDao;
 import stock.dao.StockDaoImpl;
+import store.common.config.StoreConfig;
 
 public class StockDaoTest {
 
@@ -10,11 +11,20 @@ public class StockDaoTest {
 		
 		StockDao dao = StockDaoImpl.getInstance();
 		
-		StockVO stock = new StockVO();
-		stock.setMenuId(42);
-		stock.setAmount(20);
-		stock.setMaker("제조사A");
-		dao.insert(stock);
+		int menuIdStartIdx = 41;
+		int menuIdEndIdx = 86;
+		
+		for (int i = menuIdStartIdx; i < menuIdEndIdx; i++) {
+			StockVO stock = new StockVO();
+			stock.setMenuId(i);
+			stock.setAmount(10);
+			stock.setMaker("제조사A");
+			stock.setStoreId(StoreConfig.getStoreId());
+			dao.insert(stock);
+			
+			stock.setMaker("제조사B");
+			dao.insert(stock);
+		}
 		
 		System.out.println("완료");
 		
